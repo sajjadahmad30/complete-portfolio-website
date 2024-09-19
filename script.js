6637dd2d5d13130ddb48925fa01079cd08dc8d85
sajjadahmad30 <sajjadahmad5578@gmail.com>
2024-09-19 09:14:37 +0500

    header section styling and media query and js done

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 let humburger=document.querySelector(".humburger")
 let mobileMenu=document.querySelector(".nav-list ul")
+let menuItem=document.querySelectorAll(".nav-list ul li a")
+let header=document.querySelector(".header")
+
 
 humburger.addEventListener('click',()=>{
     humburger.classList.toggle("active");
     mobileMenu.classList.toggle("active");
+});
+
+
+menuItem.forEach((item)=>{
+    item.addEventListener('click',()=>{
+        humburger.classList.toggle("active")
+        mobileMenu.classList.toggle("active")
+    });
+})
+
+
+document.addEventListener('scroll',()=>{
+    var scroll_postions = window.scrollY;
+    if(scroll_postions > 250){
+        header.style.backgroundColor="#29323c"
+    }else{
+        header.style.backgroundColor="transparent";
+    }
 });
